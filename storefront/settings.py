e0311edcfc7d4ec8da04fb6ab67e89b67346be0d
Dynamsoft-Ethan <ethan@dynamsoft.com>
2022-12-12 18:12:36 +0800

    update

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
-    'debug_toolbar'
+    'debug_toolbar',
+    'store',
+    'tags',
+    'likes'
 ]
 
 MIDDLEWARE = [
@@ -83,8 +86,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'MyPassword'
     }
 }
 
